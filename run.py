3237de8d56c0b3fabe19e5fffb34a73165ce7796
bslevin <blogs82@yahoo.com>
2022-07-19 09:27:34 +0000

    built the read_int function

--- run.py
+++ run.py
@@ -13,7 +13,23 @@ SHIP = "S"
 GUESS = "X"
 
 
-def read_int()
+def read_int(prompt: str, min_value: int = 1, max_value: int = 5) -> int:
+"""Read an integer between a min and a max value"""
+
+    while True:
+        line = input(promt)
+        try:
+            value = int(line)
+            if  value < min_value:
+                print(f"The minimum value is {min_value}. Try again.")
+            elif value > max_value:
+                print(f"The maximum value is {max_value}. Try again.")
+            else:
+                return value
+            except ValueError:
+                print("Not a number! Go again.")
+
+
 
 class BattleshipBoard
 
