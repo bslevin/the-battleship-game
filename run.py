7a738abe43a3fc7fd8b67eef09ea6b716ea824a2
bslevin <blogs82@yahoo.com>
2022-07-19 09:14:45 +0000

    added the funtion to be used for thew game

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
 import os
 import random 
 from typing import Callable
+
+# Constants used for the board
+GUESSES_COUNT = 5
+BOARD_SIZE_X = 5
+BOARD_SIZE_Y = 5
+
+# Constants used for elements on the board
+HIDDEN = "O"
+SHIP = "S"
+GUESS = "X"
+
+
+def read_int()
+
+class BattleshipBoard
+
+def read_guess()
+
+def turn()
+
+def play_game()
+
+def main()
